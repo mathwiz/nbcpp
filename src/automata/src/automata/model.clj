956532039967ebcff7a7215da678c7eddf575629
mathwiz <mathwiz@gmail.com>
2019-02-14 22:25:22 -0500

    automata

--- src/automata/src/automata/model.clj
+++ src/automata/src/automata/model.clj
@@ -21,9 +21,22 @@
   ([len rule init] (automaton-new rule (recycle-list len () init))))
 
 
+(defn evolve-cell [cells index rule]
+  (let [left   (if (= index 0) (nth cells (- (count cells) 1)) (nth cells (dec index)))
+        center (nth cells index)
+        right  (if (= index (- (count cells) 1)) 0 (nth cells (inc index)))]
+    (rule left center right)))
+
+
+(defn evolve-cells [cells rule index acc]
+  (cond (= it (count cells)) acc
+    :else                    (recur cells rule (inc index) (cons (evolve-cell cells index rule) acc))))
+
+
 (defn evolve [automaton]
   "Computes the next iteration of automaton"
-  (automaton-new (get automaton :rule) (get automaton :cells)))
+  (automaton-new (get automaton :rule)
+                 (get automaton :cells)))
 
 
 (defn R0 [l c r]
@@ -31,11 +44,15 @@
 
 
 (defn R30 [l c r]
-  (or (and (= l 1) (= c 0) (= r 0))))
+  (if (or (and (= l 1) (= c 0) (= r 0)))
+    1
+    0))
 
 
 (defn R90 [l c r]
-  (or (and (= l 1) (= c 0) (= r 0))))
+  (if (or (and (= l 1) (= c 0) (= r 0)))
+    1
+    0))
 
 
 (defn make-rule [num]
